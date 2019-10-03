28d865ac140c90def16a334ed10228fa7307c0ca
sumitrao007 <raokhande.sumit@gmail.com>
2019-10-03 16:16:10 +0530

    Typescript Day1

---
+++ Day1/demo1.js
@@ -0,0 +1,93 @@
+console.log("Welcome To My Lecture!!!!!!");
+var a = 10;
+console.log(a);
+var a_b;
+var a$b;
+var temp = 20.5;
+console.log("Value is " + temp);
+var temp1 = 'Sumit';
+console.log("Name Is " + temp1);
+var temp3 = true;
+console.log(temp3);
+console.log("\n--------------------------------------\nName is " + temp1 + " fbdhfvbsdhfvbdsj\n");
+console.log("---------------------- name is " + temp1 + "sdhgdhfdhsad");
+var name1 = 10;
+var temp4;
+temp4 = "vsdhfbsd";
+console.log(temp4);
+temp4 = 20;
+console.log(temp4);
+//  name1="xfsdf";
+console.log(typeof (name1));
+console.log(typeof temp4);
+temp4 = "Sumit";
+console.log(typeof temp4);
+var temp5;
+console.log(temp5);
+var temp6;
+console.log(temp6);
+var temp7;
+console.log(temp7);
+var temp8 = null;
+console.log(temp8);
+var temp9;
+//Type Assertion
+//(<string> temp9).
+// (temp9 as string).
+//Operators
+//arithmatocal,logical,bitwise,relational,assignment,conditional
+//increment,decrement
+var a = 10;
+var b = 20;
+console.log(a + b);
+var a1;
+var a2;
+a1 = 10;
+a2 = 30;
+console.log(a1 + a2);
+var temp10 = a1 > 0 ? a1 : a2;
+console.log(temp10);
+var temp11 = a1 > 0 ? a2 < 0 ? "hi" : "bye" : a2;
+console.log(temp11);
+if (a1 == 0) {
+    console.log("HI!!!!");
+}
+else {
+    console.log("Bye...");
+    if (a2 > 0) {
+        console.log("Bye... In Nested loop");
+    }
+}
+if (a1 > a2) {
+}
+else if (a2 > 0) {
+}
+// for(var i:number=0;i<5;i++){
+//     console.log(`
+//     ---------------------------
+//     ${i}
+//     `);
+// }
+// for (let index = 0; index < 5; index++) {
+//    console.log(`
+//    ------------------------------
+//    ${index} ${a1}
+//    `)
+// }
+//console.log(index);
+// for (let index = 0; index < 5; index++) {
+//     console.log(`
+//     ------------------------------
+//     ${index} 
+//     `)
+//     for (let i = 0; i < 2; i++) {
+//         console.log(`
+//         ------ I Value Is ${i}
+//         `);
+//     } 
+//  }
+var pi = 3.14;
+for (var index = 0; index < 3; index++) {
+    var element = index;
+    console.log(element);
+}
